04284e2b31d9e20d20f0bd0089b4da5737f4de2e
cats-marin <212922539+cats-marin@users.noreply.github.com>
2025-08-19 13:51:14 -0500

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kanditioned",
-    version="1.0.0",
+    version="1.0.1",
     author="cats-marin",
     description="Fast, Conditioned KAN",
     long_description=open("README.md").read(),
