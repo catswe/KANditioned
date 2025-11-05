be62e9a49ed638a61c8f6973dd7fce6194cea084
cats-marin <212922539+cats-marin@users.noreply.github.com>
2025-11-04 19:10:40 -0600

    Update to use less memory

--- minimal.py
+++ minimal.py
@@ -8,7 +8,7 @@ class KANLayer(nn.Linear):
     """Zero-centered spline version. Minimal version with no spline visualization, custom init, or error checking.
     TODO: Implement custom backward pass to reduce memory pressure and 2:4 sparse semi-structured tensor."""
 
-    def __init__(self, in_features: int, out_features: int, num_control_points: int = 5, spline_width: float = 2.0, bias: bool=True):
+    def __init__(self, in_features: int, out_features: int, num_control_points: int = 5, spline_width: float = 3.0, bias: bool=True):
         super().__init__(in_features, out_features, bias=bias)
         if num_control_points % 2 == 0 or num_control_points < 3:
             raise ValueError("num_control_points must be an odd value greater than or equal to 3.")
@@ -16,7 +16,7 @@ class KANLayer(nn.Linear):
         self.num_control_points = num_control_points
         self.spline_width = spline_width
 
-        self.register_buffer("feature_offset", torch.arange(in_features).view(1, -1) * (num_control_points - 1))
+        self.register_buffer("feature_offset", torch.arange(in_features, dtype=torch.int32).view(1, -1) * (num_control_points - 1))
         self.weight = nn.Parameter(torch.zeros(out_features, in_features * (num_control_points - 1)))
 
     def forward(self, input):
@@ -25,12 +25,12 @@ class KANLayer(nn.Linear):
         idx_float = x.floor().clamp_(0, self.num_control_points - 2)
         interpolation_weight = x - idx_float
 
-        dense_matrix = torch.zeros(x.size(0), self.weight.size(1), device=x.device)
+        dense_matrix = torch.zeros(x.size(0), self.weight.size(1), device=x.device, dtype=x.dtype)
 
         mid_index = self.num_control_points // 2
-        idx = idx_float.long() - (idx_float >= mid_index).long() + self.feature_offset
+        idx = idx_float.to(torch.int32) - (idx_float >= mid_index).to(torch.int32) + self.feature_offset
 
-        dense_matrix.scatter_(1, idx, (1 - interpolation_weight) * (idx_float != mid_index))
-        dense_matrix.scatter_(1, idx + 1, interpolation_weight * (idx_float != (mid_index - 1)))
+        dense_matrix.scatter_(1, idx, (1 - interpolation_weight).type_as(x).masked_fill_(idx_float == mid_index, 0))
+        dense_matrix.scatter_(1, idx + 1, interpolation_weight.type_as(x).masked_fill_(idx_float == (mid_index - 1), 0))
 
         return F.linear(dense_matrix, self.weight, self.bias)
